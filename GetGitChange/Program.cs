ba821eb91dfeb0689877a9142d56022d827d8b41
kesshei <dagang520@qq.com>
2025-08-08 16:47:11 +0800

    提交

---
+++ GetGitChange/Program.cs
@@ -0,0 +1,56 @@
+﻿using LibGit2Sharp;
+
+namespace GetGitChange
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("please give args  source  target ,ex: exe E:\\git E:\\save");
+                return;
+            }
+            string repoPath = args[0];
+            string targetPath = args.Length > 1 ? args[1] : Path.Combine(AppContext.BaseDirectory, "temp");
+
+            using (var repo = new Repository(repoPath))
+            {
+                RepositoryStatus status = repo.RetrieveStatus();
+
+                List<string> deletePaths = new List<string>();
+
+                var list = status.Where(t => t.State == FileStatus.NewInWorkdir || t.State == FileStatus.ModifiedInWorkdir || t.State == FileStatus.DeletedFromWorkdir);
+                foreach (StatusEntry entry in status.Where(t => t.State != FileStatus.Ignored))
+                {
+                    if (entry.State == FileStatus.DeletedFromWorkdir)
+                    {
+                        deletePaths.Add(entry.FilePath);
+                    }
+                    else
+                    {
+                        var path = Path.Combine(repo.Info.WorkingDirectory, entry.FilePath);
+                        if (File.Exists(path))
+                        {
+                            var newPath = Path.Combine(targetPath, entry.FilePath);
+                            var dir = Path.GetDirectoryName(newPath);
+                            if (dir != null && !Directory.Exists(dir))
+                            {
+                                Directory.CreateDirectory(dir);
+                            }
+                            File.Copy(path, newPath, true);
+                            Console.WriteLine($"Copied: {path} to {newPath}");
+                        }
+                    }
+                }
+                if (deletePaths.Any())
+                {
+                    File.WriteAllText(Path.Combine(targetPath, "delete.txt"), string.Join(Environment.NewLine, deletePaths));
+                }
+                Console.WriteLine("Changes copied to target directory.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # GetGitChange
 实现获取git仓库的更改并保存到另外一个目录下
+
+
+# 使用方法
+GetGitChange.exe  <source> <destination>
+<source> 为git仓库的路径
+<destination> 为保存更改的路径
+
+常用命令:
+
+## 命令1
+```
+GetGitChange.exe  E:\\git E:\\save
+```
+会把E:\\git仓库的更改保存到E:\\save目录下
+
+## 命令2
+
+```
+GetGitChange.exe  E:\\git
+
+```
+会把E:\\git仓库的更改保存到当前命令目录下temp目录里
+
+
+## 删除的文件
+delete.txt 里面的文件需要删除
